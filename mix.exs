5acea7c746d94091f9b178e8e84a91713371df24
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-01-07 23:15:05 +0100

    prepare to publish

--- mix.exs
+++ mix.exs
@@ -3,7 +3,9 @@ defmodule Reaxt.Mixfile do
 
   def project do
     [app: :reaxt,
-     version: "0.0.1",
+     version: "0.1.0",
+     description: description,
+     package: package,
      elixir: "~> 1.0",
      deps: deps]
   end
@@ -25,5 +27,30 @@ defmodule Reaxt.Mixfile do
      {:cowboy,"~> 1.0.0"},
      {:plug,"~> 0.9.0"},
      {:poison,"1.3.0"}]
+  end
+
+  defp package do
+    [ contributors: ["Arnaud Wetzel"],
+      licenses: ["The MIT License (MIT)"],
+      links: [ { "GitHub", "https://github.com/awetzel/reaxt" } ] ]
+  end
+
+  defp description do
+    """
+    Use your *react* components into your elixir application, using webpack compilation, so :
+    
+    - An isomorphic ready library (SEO/JS are now nice together), but with Elixir on the server side
+    - Just a Library, with a minimum constraint about your application organization and layout :
+      - use any javascript compiled language
+      - use any javascript routing logic or library
+      - you can use JS React rendered component only for parts of your webpage
+    - Nice fluent dev workflow, with :
+      - combined stacktrace : elixir | javascript
+      - hot loading on both server and browser
+      - NPM/Webpack as the only config for respectively dependencies/compilation
+      - A cool UI to have an overview of your compiled javascript application
+      - You do not have to think about the server side Javascript configuration, 
+        just write a webpack conf for the browser, and it is ready to use.
+    """
   end
 end
