318fcdad52bc4cd3282bb642d33d2a1d6cea3d12
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-01-07 12:04:33 +0100

    rename reaxt extension dir

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Npm.Install do
   @shortdoc "`npm install` in web_dir + npm install server side dependencies"
   def run(_args) do
     System.cmd("npm",["install"], into: IO.stream(:stdio, :line), cd: "web")
-    System.cmd("npm",["install","#{:code.priv_dir(:reaxt)}/react_server"], into: IO.stream(:stdio, :line), cd: "web")
+    System.cmd("npm",["install","#{:code.priv_dir(:reaxt)}/commonjs_reaxt"], into: IO.stream(:stdio, :line), cd: "web")
   end
 end
 
