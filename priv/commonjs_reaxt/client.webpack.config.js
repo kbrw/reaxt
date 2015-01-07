ca2b921dfd1b800b13f2e0da9399987bc4c0d9ed
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-01-07 12:08:22 +0100

    clean comments

--- priv/commonjs_reaxt/client.webpack.config.js
+++ priv/commonjs_reaxt/client.webpack.config.js
@@ -1,7 +1,3 @@
 var client_config = require("./../../webpack.config.js")
-//client_config.module.cssLoaders = client_config.module.cssLoaders.map(function(e){
-//  e.loader = "style!" + e.loader
-//  return e
-//})
-//client_config.module.loaders = (client_config.module.loaders || []).concat(client_config.module.cssLoaders)
+//customize client config
 module.exports = client_config
--- priv/commonjs_reaxt/server.webpack.config.js
+++ priv/commonjs_reaxt/server.webpack.config.js
@@ -3,9 +3,4 @@ client_config.entry = "reaxt/react_server"
 client_config.output = { path: '../priv', filename: 'server.js' }
 client_config.target = "node"
 client_config.externals = {}
-//client_config.module.cssLoaders = client_config.module.cssLoaders.map(function(e){
-//  e.loader = __dirname + "/style-collector!" + e.loader
-//  return e
-//})
-//client_config.module.loaders = (client_config.module.loaders || []).concat(client_config.module.cssLoaders)
 module.exports = client_config
